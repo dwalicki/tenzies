104e350371bda17fe00793933094c0aff6a709bb
Daniel <danielrwalicki@gmail.com>
2023-02-20 00:04:15 -0800

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        Coded with{" "}
+        <span role="img" aria-label="heart">
+          ❤️
+        </span>{" "}
+        by{" "}
+        <h2>
+          Daniel W
+        </h2>
+      </small>
+    </footer>
+  );
+}
